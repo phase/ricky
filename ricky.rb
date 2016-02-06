ff8bdd3c7b22cc40e808b1eb4696ccac00e7b81b
phase <ph4sesaber@gmail.com>
2016-02-06 02:24:32 -0800

    Theme loading

--- ricky.rb
+++ ricky.rb
@@ -3,7 +3,15 @@
 require "webrick"
 require "github/markup"
 
-class MyServlet < WEBrick::HTTPServlet::AbstractServlet
+def render_page(file, extension)
+    markup = GitHub::Markup.render(file + extension, File.read("wiki/" + file + extension))
+    theme = File.read("theme.html")
+    theme = theme.gsub! "{% title %}", file
+    theme = theme.gsub! "{% content %}", markup
+    return theme
+end
+
+class RickyServlet < WEBrick::HTTPServlet::AbstractServlet
     def do_GET (request, response)
         file = request.path[1..-1]
         extensions = [".md", ".wiki", ".creole", ".org"]
@@ -11,7 +19,7 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
             if File.file?("wiki/" + file + extension)
                 response.status = 200
                 #response.content_type = "text/plain" #test output
-                response.body = GitHub::Markup.render(file + extension, File.read("wiki/" + file + extension))
+                response.body = render_page file, extension
             end
         end
     end
@@ -19,7 +27,7 @@ end
 
 server = WEBrick::HTTPServer.new(:Port => 80)
 
-server.mount "/", MyServlet
+server.mount "/", RickyServlet
 
 trap("INT") {
     server.shutdown
